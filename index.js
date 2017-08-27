859400b246798908e386a6e54ab1a0a3b9698a0d
Jacob Zuo <chopinsky@live.com>
2017-08-27 09:02:30 -0500

    supporting none null pad strings as filler

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-exports.padStart = (str, targetLength) => {
+/*
+ * pad-string
+ * https://github.com/Chopinsky/pad-string
+ *
+ * Copyright (c) 2017 Jacob Zuo
+ * Licensed under the MIT license.
+ */
+
+exports.padStart = function (str, targetLength) {
   var strLength = ~~targetLength;
   if (isNaN(strLength) || strLength === 0) {
     return str;
   }
 
   if (!str || str.length === 0) {
-    return ' '.repeat(strLength);
+    return getPaddingFiller(strLength);
   }
 
   var target = (typeof str === 'string') ? str : str.toString();
@@ -14,17 +22,17 @@ exports.padStart = (str, targetLength) => {
     return str;
   }
 
-  return ' '.repeat(strLength - target.length) + target;
+  return getPaddingFiller(strLength - target.length) + target;
 }
 
-exports.padEnd = (str, targetLength) => {
+exports.padEnd = function (str, targetLength) {
   var strLength = ~~targetLength;
   if (isNaN(strLength) || strLength === 0) {
     return str;
   }
 
   if (!str) {
-    return ' '.repeat(strLength);
+    return getPaddingFiller(strLength);
   }
 
   var target = (typeof str === 'string') ? str : str.toString();
@@ -33,17 +41,17 @@ exports.padEnd = (str, targetLength) => {
     return target;
   }
 
-  return target + ' '.repeat(strLength - target.length);
+  return target + getPaddingFiller(strLength - target.length);
 }
 
-exports.padBothEnds = (str, targetLength) => {
+exports.padBothEnds = function (str, targetLength) {
   var strLength = ~~targetLength;
   if (isNaN(strLength) || strLength === 0) {
     return str;
   }
 
   if (!str) {
-    return ' '.repeat(strLength);
+    return getPaddingFiller(strLength);
   }
   
   var target = (typeof str === 'string') ? str : str.toString();
@@ -53,8 +61,23 @@ exports.padBothEnds = (str, targetLength) => {
   }
 
   var extraPads = strLength - target.length;
-  var padFront = Math.ceil(extraPads/2);
-  var padTail = Math.floor(extraPads/2);
+  var padFront = Math.ceil(extraPads / 2);
+  var padTail = Math.floor(extraPads / 2);
 
-  return ' '.repeat(padFront) + str + ' '.repeat(padTail);
+  return getPaddingFiller(padFront) + str + getPaddingFiller(padTail);
+}
+
+var getPaddingFiller = function (len, padStr) {
+  if (!len) {
+    return '';
+  }
+
+  if (!padStr) {
+    return ' '.repeat(len);
+  }
+
+  var repeat = Math.floor(padStr.length / strLen);
+  var remainder = padStr.length % strLen;
+
+  return padStr.repeat(repeat) + padStr.substring(0, remainder);
 }
